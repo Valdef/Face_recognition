dfca630b716e5733f4783a80d63007ec9b51a564
valentin <valentin.defayet@gmail.com>
2026-03-12 17:07:49 +0000

    finished main.py. can now recognise any face that we first include in Images folder.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import face_recognition
 import os 
 from tqdm import tqdm
 
-path = 'Val_dataset'
+path = 'Images'
 images = []
 classNames = []
 myList = os.listdir(path)
@@ -12,7 +12,8 @@ myList = os.listdir(path)
 for cls in myList:
     curImg = cv2.imread(f'{path}/{cls}')
     images.append(curImg)
-    classNames.append(os.path.splitext(cls)[0])
+    classNames.append(os.path.splitext(cls)[0].split('_')[0])
+    print(classNames)
 
 def findEncodings(images):
     encodeList = []
@@ -25,6 +26,33 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 print('Face Encoded: ', len(encodeListKnown))
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDist = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDist)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 =  y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0,255,0),2)
+            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0,255,0), cv2.FILLED)
+            cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
